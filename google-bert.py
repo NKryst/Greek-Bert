d431491c47e9d3ca638a6a88713189b0cbc72cd2
NKryst <nickrystallis@outlook.com.gr>
2020-07-30 13:38:53 +0300

    Updare

--- google-bert.py
+++ google-bert.py
@@ -2,8 +2,6 @@ import torch
 import pandas as pd
 from tqdm.notebook import tqdm
 
-data = open('C:\Users\nickr\Desktop\Projects\R_Twitter\Twitter\tweets.csv')
-data.encode('utf-8').strip()
 df = pd.read_csv(r'C:\Users\nickr\Desktop\Projects\R_Twitter\Twitter\tweets.csv',
 names =['id','text','Anger','Disgust','Fear','Happiness','Sadness','Surprise']
 )
