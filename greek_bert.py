6b3ef2cc455de1e748cc0b59f20b71be75172d3e
Nk <17711093+NKryst@users.noreply.github.com>
2020-10-17 12:20:45 +0300

    Initiate script

---
+++ greek_bert.py
@@ -0,0 +1,6 @@
+
+from transformers import AutoTokenizer, AutoModelForPreTraining
+
+tokenizer = AutoTokenizer.from_pretrained("nlpaueb/bert-base-greek-uncased-v1")
+
+model = AutoModelForPreTraining.from_pretrained("nlpaueb/bert-base-greek-uncased-v1")
